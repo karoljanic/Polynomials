d1baed61a67dacfd230c54c2bb1fb00b48a300b3
karoljanic <karoljanic@gmail.com>
2020-06-06 00:05:47 +0200

    update

---
+++ drawing_test.py
@@ -0,0 +1,18 @@
+import drawing_class as draw
+
+drawing = draw.Drawing()
+
+drawing.draw_test_frame()
+drawing.draw_0()
+
+drawing.draw_test_frame()
+drawing.draw_root_symbol(2)
+
+drawing.draw_test_frame()
+drawing.draw_3()
+
+drawing.draw_test_frame()
+drawing.draw_5()
+
+drawing.draw_test_frame()
+drawing.window.exitonclick()
